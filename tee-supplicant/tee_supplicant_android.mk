58e065abb68168aa2b6c661e18ca36fee38dd129
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2018-10-31 19:11:56 +0200

    Android.mk: Added tee-supp_recovery terget for recovery mode.
    Added static executable for recovery (/sbin/tee-supp_recovery)
    
    Jira-Id: ADM-1529
    
    Change-Id: Icff78446c09d5d8ceaad91100390e36983833c33
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>

--- tee-supplicant/tee_supplicant_android.mk
+++ tee-supplicant/tee_supplicant_android.mk
@@ -4,20 +4,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_CFLAGS += $(optee_CFLAGS)
-
-LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_SUPP_LOG_LEVEL) \
-                -DBINARY_PREFIX=\"TEES\" \
-                -DTEEC_LOAD_PATH=\"$(CFG_TEE_CLIENT_LOAD_PATH)\" \
-                -DTEE_FS_PARENT_PATH=\"$(CFG_TEE_FS_PARENT_PATH)\"
-
 LOCAL_CFLAGS += -DANDROID_BUILD -DDEBUG=1
 LOCAL_CFLAGS += $(CFLAGS)
 
 LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_SUPP_LOG_LEVEL)
 LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEES\"
 LOCAL_CFLAGS += -DTEEC_LOAD_PATH=\"$(CFG_TEE_CLIENT_LOAD_PATH)\"
-LOCAL_CFLAGS += -DTEE_FS_PARENT_PATH=\"$(CFG_TEE_FS_PARENT_PATH)\"
+LOCAL_CFLAGS += -DTEE_FS_PARENT_PATH=\"$(CFG_TEE_FS_ANDROID_PARENT_PATH)\"
 
 ifeq ($(CFG_TA_TEST_PATH),y)
 LOCAL_CFLAGS += -DCFG_TA_TEST_PATH=1
@@ -27,7 +22,6 @@ ifeq ($(CFG_GP_SOCKETS),y)
 LOCAL_CFLAGS += -DCFG_GP_SOCKETS=1
 endif
 
-
 LOCAL_SRC_FILES += src/handle.c \
                    src/tee_supp_fs.c \
                    src/tee_supplicant.c \
@@ -35,7 +29,6 @@ LOCAL_SRC_FILES += src/handle.c \
                    src/rpmb.c \
                    src/sha2.c \
                    src/hmac_sha2.c
-
 
 ifeq ($(CFG_GP_SOCKETS),y)
 LOCAL_SRC_FILES += src/tee_socket.c
@@ -54,6 +47,58 @@ endif
 
 LOCAL_MODULE := tee-supp
 LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+# ----------------------------------------------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_CFLAGS += $(optee_CFLAGS)
+LOCAL_CFLAGS += -DANDROID_BUILD -DDEBUG=1
+LOCAL_CFLAGS += $(CFLAGS)
+
+LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_SUPP_LOG_LEVEL)
+LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEES\"
+LOCAL_CFLAGS += -DTEEC_LOAD_PATH=\"$(CFG_TEE_CLIENT_LOAD_PATH)\"
+LOCAL_CFLAGS += -DTEE_FS_PARENT_PATH=\"$(CFG_TEE_FS_RECOVERY_PARENT_PATH)\"
+
+ifeq ($(CFG_TA_TEST_PATH),y)
+LOCAL_CFLAGS += -DCFG_TA_TEST_PATH=1
+endif
+
+ifeq ($(CFG_GP_SOCKETS),y)
+LOCAL_CFLAGS += -DCFG_GP_SOCKETS=1
+endif
+
+LOCAL_SRC_FILES += src/handle.c \
+                   src/tee_supp_fs.c \
+                   src/tee_supplicant.c \
+                   src/teec_ta_load.c \
+                   src/rpmb.c \
+                   src/sha2.c \
+                   src/hmac_sha2.c
+
+
+ifeq ($(CFG_GP_SOCKETS),y)
+LOCAL_SRC_FILES += src/tee_socket.c
+endif
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../public \
+    $(LOCAL_PATH)/../libteec/include \
+    $(LOCAL_PATH)/src
+
+LOCAL_STATIC_LIBRARIES := libteec libm libz libc libdl
+
+ifeq ($(CFG_TA_GPROF_SUPPORT),y)
+LOCAL_SRC_FILES += src/gprof.c
+LOCAL_CFLAGS += -DCFG_TA_GPROF_SUPPORT
+endif
+
+LOCAL_MODULE := tee-supp_recovery
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
